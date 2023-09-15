4c74759a3923b77474867bfbb8bd86d9f5662aa1
luisperez9401 <31638158+luisperez9401@users.noreply.github.com>
2023-09-15 08:49:39 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -106,6 +106,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-file-pdf',
+        titulo: 'Descargar PDF',
+        download: 'downloads/331502_CF22_DU.pdf',
+      },
+      {
         icono: 'fas fa-download',
         titulo: 'Descargar material',
         download: 'downloads/material.zip',
